8165434cea01ebccdb9303a7a53d709e200a2c05
Grinsanity <thepizzapop22@outlook.com>
2015-11-26 18:49:15 -0500

    Created script js

---
+++ script.js
@@ -0,0 +1,6 @@
+ var userChoice1 = confirm("Are you ready to play?");
+if (userChoice1 === true) {
+    var directionChoice1 = prompt("You woke up alone. Nothing but the vast forest that seemed to extend for miles. Man. You sure screwed up this time. What did you do? Did you [go forward], [go left], or [go right]?"); //The correct direction will be "go left"
+}
+if (directionChoice1 === "go forward") {
+    prompt("You began to walk forward. You were walking for what felt like hours, but was really only fifteen minutes.
